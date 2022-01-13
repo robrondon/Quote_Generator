e497cbc51ab8ea8a5ac1212ad12861bc5e95a113
Robert Rondon <robrondon11@gmail.com>
2022-01-13 18:45:36 -0300

    get quotes from api, tweet functionality

--- script.js
+++ script.js
@@ -1,32 +1,65 @@
+const quoteContainer = document.querySelector('#quote-container');
+const quoteText = document.querySelector('#quote');
+const quoteAuthor = document.querySelector('#author');
+const twitterBtn = document.querySelector('#twitter');
+const newQuoteBtn = document.querySelector('#new-quote');
+
 let quotesList = [];
 
-//Generate random quote
-const newQuote = function () {
-  //Pick random index from quotesList array
-  let randomIndex = Math.floor( Math.random() * quotesList.length );
-  const quote = quotesList[randomIndex];
-  console.log( quote );
-}
+// Check if author is blank and replace it with 'unknown'
+const checkAuthor = function (quote) {
+  if (!quote.author) {
+    quoteAuthor.textContent = 'Unknown';
+  } else {
+    quoteAuthor.textContent = quote.author;
+  }
+};
 
-//Generate a Quote from a local array in case of error
-const backupQuote = function () {
-  let randomIndex = Math.floor( Math.random() * localQuotes.length );
-  const quote = localQuotes[randomIndex];
-  console.log( quote );
-}
+// Reduce the size of the quote text if exceeds 120 char
+const checkQuoteText = function (quote) {
+  if (quote.text.length > 120) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+  quoteText.textContent = quote.text;
+};
+
+// Generate random quote
+const newQuote = function (quotes) {
+  // Pick random index from quotesList array
+  let randomIndex = Math.floor(Math.random() * quotes.length);
+  const quote = quotes[randomIndex];
+  checkAuthor(quote);
+  checkQuoteText(quote);
+};
 
 // Get Quotes from API
 const getQuotes = async function () {
-  const apiUrl = 'https://type.fit/api/quotes2';
+  const apiUrl = 'https://type.fit/api/quotes';
   try {
-    const response = await fetch( apiUrl );
+    const response = await fetch(apiUrl);
     quotesList = await response.json();
-    newQuote();
-  } catch ( error ) {
-    backupQuote();
-    console.log( error );
+    newQuote(quotesList);
+  } catch (error) {
+    console.log(error);
+    // If fetch fails, we use local array quotes from quotes.js
+    quotesList = localQuotes;
+    newQuote(quotesList);
   }
-}
+};
+
+// Tweet quote
+const tweetQuote = function () {
+  const tweetUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${quoteAuthor.textContent}`;
+  window.open(tweetUrl, '_blank');
+};
+
+// Event Listeners
+twitterBtn.addEventListener('click', tweetQuote);
+newQuoteBtn.addEventListener('click', () => {
+  newQuote(quotesList);
+});
 
 // On load!
 getQuotes();
